646c1478f35b3a8cf84620f58823d0a2e98070d9
Emmanuel DROUET <emmanuel.drouet@gmail.com>
2018-06-24 16:05:20 +0200

    don't display "browse tree" if no locationId on request

--- src/bundle/EventListener/ConfigureMenuListener.php
+++ src/bundle/EventListener/ConfigureMenuListener.php
@@ -12,33 +12,44 @@ class ConfigureMenuListener implements TranslationContainerInterface
 {
     const ITEM__BROWSE_TRE = 'sidebar_left__browse_tree';
 
+    /** @var RequestStack  */
+    protected $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
     /**
      * @param ConfigureMenuEvent $event
      */
     public function onMenuConfigure(ConfigureMenuEvent $event)
     {
-        $menu = $event->getMenu();
+        $request = $this->requestStack->getCurrentRequest();
+        if ($request->attributes->has('locationId')) {
+            $menu = $event->getMenu();
 
-        $menu->addChild(
-            self::ITEM__BROWSE_TRE,
-            [
-                'extras' => ['icon' => 'list'],
-                'attributes' => [
-                    'class' => 'ez-btn--extra-actions-left btn--tree-browse',
-                    'data-actions' => 'browse-tree',
-                ],
-            ]
-        );
+            $menu->addChild(
+                self::ITEM__BROWSE_TRE,
+                [
+                    'extras' => ['icon' => 'list'],
+                    'attributes' => [
+                        'class' => 'ez-btn--extra-actions-left btn--tree-browse',
+                        'data-actions' => 'browse-tree',
+                    ],
+                ]
+            );
 
-        $children = $menu->getChildren();
-        $order = array_keys($children);
-        $oldPosition = array_search(self::ITEM__BROWSE_TRE, $order);
-        unset($order[$oldPosition]);
+            $children = $menu->getChildren();
+            $order = array_keys($children);
+            $oldPosition = array_search(self::ITEM__BROWSE_TRE, $order);
+            unset($order[$oldPosition]);
 
-        $order = array_values($order);
+            $order = array_values($order);
 
-        array_splice($order, 1, 0, self::ITEM__BROWSE_TRE);
-        $menu->reorderChildren($order);
+            array_splice($order, 1, 0, self::ITEM__BROWSE_TRE);
+            $menu->reorderChildren($order);
+        }
     }
 
     /**
